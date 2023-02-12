41da712b89b6ce426f75f4add39f2add5d54549a
lucasdias38 <lucas_rodrigues3838@hotmail.com>
2023-02-12 00:16:48 -0300

    Inicio Projeto

---
+++ src/test/java/com/agrotis/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotis.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
